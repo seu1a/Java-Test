a31c3ff71d7e3ca10e716ea04d740636632b60a6
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-11-26 15:30:31 +0900

    fix: sql 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO member_entity (id, username, password, role) VALUES (1, 'admin', '$2a$10$3DVujfFeizZfj9eLNOE5Ju6D45wWTU/CyY/m5VSFmGsYCZHjBrtU.', 'ADMIN');
+INSERT INTO member_entity (id, username, password, role) VALUES (1001, 'admin', '$2a$10$3DVujfFeizZfj9eLNOE5Ju6D45wWTU/CyY/m5VSFmGsYCZHjBrtU.', 'ADMIN');
 
 INSERT INTO diary (id, title, content, member_id, created_at)
 VALUES
-    (1001, 'First Diary Entry', 'This is the first diary content.', 1, NOW()),
-    (1002, 'Second Diary Entry', 'This is the second diary content.', 1, NOW()),
-    (1003, 'Third Diary Entry', 'This is the third diary content.', 1, NOW());
+    (1001, 'First Diary Entry', 'This is the first diary content.', 1001, NOW()),
+    (1002, 'Second Diary Entry', 'This is the second diary content.', 1001, NOW()),
+    (1003, 'Third Diary Entry', 'This is the third diary content.', 1001, NOW());
